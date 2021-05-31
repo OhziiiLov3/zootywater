9ea5b043d2f5f38c4b9e9da278b58f3633715c74
kbaskerville <klbaskerille0520@gmail.com>
2021-05-31 11:52:46 -0700

    created customer model and superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class CustomerProfile(models.Model):
+
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=200)
+    join_date = models.DateField(default=date.today())
+    is_subscribe = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
