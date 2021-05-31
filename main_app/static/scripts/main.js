40c93e6fc8957035f9e629f7a7976c9fc1f13724
kbaskerville <klbaskerille0520@gmail.com>
2021-05-31 00:04:25 -0700

    added subscribe form to base

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -5,8 +5,8 @@ const cardContainer = document.querySelector(".card-container");
 // moving Animation Listener
 
 cardContainer.addEventListener("mousemove",(event) => {
-    let xAxis = (window.innerWidth / 2 - event.pageX) / 29;
-    let yAxis = (window.innerHeight/ 2 - event.pageY) / 29;
+    let xAxis = (window.innerWidth / 2 - event.pageX) / 28;
+    let yAxis = (window.innerHeight/ 2 - event.pageY) / 28;
 
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     console.log("hey");
