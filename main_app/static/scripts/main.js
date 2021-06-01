7b567fbaf3cd19869c07d7818f335beb3c2abb1e
kbaskerville <klbaskerille0520@gmail.com>
2021-06-01 12:20:33 -0700

    added auto mute to video

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -13,18 +13,3 @@ cardContainer.addEventListener("mousemove",(event) => {
     console.log("hey");
 });
 
-/* USER PROFILE PAGE VIDEO CARD */
-
-// movement for card animation 
-const profileVideo = document.querySelector(".profilevideo");
-const videoContainer = document.querySelector(".videocontainer");
-// moving Animation Listener
-
-videoContainer.addEventListener("mousemove", (event) => {
-    let xAxis = (window.innerWidth / 2 - event.pageX) / 28;
-    let yAxis = (window.innerHeight / 2 - event.pageY) / 28;
-
-    profileVideo.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-    console.log("hey");
-});
-
