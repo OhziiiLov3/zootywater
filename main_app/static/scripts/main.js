1dcdc98dbbd60d5b08e4d5ff719e6fdc87dd0f54
kbaskerville <klbaskerille0520@gmail.com>
2021-06-01 12:17:12 -0700

    added video and fixed css issue

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,3 +1,5 @@
+/* ABOUT PAGE CARDS */
+
 // movement for card animation 
 const card = document.querySelector(".card");
 const cardContainer = document.querySelector(".card-container");
@@ -10,3 +12,19 @@ cardContainer.addEventListener("mousemove",(event) => {
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     console.log("hey");
 });
+
+/* USER PROFILE PAGE VIDEO CARD */
+
+// movement for card animation 
+const profileVideo = document.querySelector(".profilevideo");
+const videoContainer = document.querySelector(".videocontainer");
+// moving Animation Listener
+
+videoContainer.addEventListener("mousemove", (event) => {
+    let xAxis = (window.innerWidth / 2 - event.pageX) / 28;
+    let yAxis = (window.innerHeight / 2 - event.pageY) / 28;
+
+    profileVideo.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    console.log("hey");
+});
+
