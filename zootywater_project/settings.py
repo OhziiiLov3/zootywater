241f2930740ce49f413c589236f1f29b738c5faf
ohziiilov3 <klbaskerville0520@gmail.com>
2023-08-26 00:31:27 -0700

    deployment

--- zootywater_project/settings.py
+++ zootywater_project/settings.py
@@ -34,6 +34,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Example: for local development
+    "https://zootywater-production.up.railway.app",  # Add this line
+]
+
 
 # Application definition
 
