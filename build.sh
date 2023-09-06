f73fc3ccf30f8e6d28c8b3a59db6a2511a1b3671
ohziiilov3 <klbaskerville0520@gmail.com>
2023-09-06 13:39:05 -0700

    updated build

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pip3 install -r requirements.txt && python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 if [[ $CREATE_SUPERUSER ]];
 then
-  python3 zootyrender/manage.py createsuperuser --no-input
+  python3 manage.py createsuperuser --no-input
 fi
 
