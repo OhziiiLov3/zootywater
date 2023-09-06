5a3fd6ff3e5345fa9e0b3333005f6cba89f243db
ohziiilov3 <klbaskerville0520@gmail.com>
2023-09-06 13:36:39 -0700

    updated build

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pip3 install -r requirements.txt && python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python3 zooty_render/manage.py createsuperuser --no-input
 fi
 
